dac9b64157b5792fef3e2208d86c392f6ba5e534
ayon008 <shariar.ayon128@gmail.com>
2025-05-21 01:43:45 +0600

    a

--- src/lib/getLeadById.js
+++ src/lib/getLeadById.js
@@ -2,16 +2,16 @@ export const revalidate = 1;
 export default async function getLeads(leads, states, id) {
     let url = ``;
     if (leads && states && id) {
-        url = `http://localhost:5000/getLeads?leadName=${leads}&states=${states}&id=${id}`;
+        url = `https://backend.janitorialappointment.com/getLeads?leadName=${leads}&states=${states}&id=${id}`;
     }
     else if (leads && states) {
-        url = `http://localhost:5000/getLeads?leadName=${leads}&states=${states}`;
+        url = `https://backend.janitorialappointment.com/getLeads?leadName=${leads}&states=${states}`;
     }
     else if (leads) {
-        url = `http://localhost:5000/getLeads?leadName=${leads}`;
+        url = `https://backend.janitorialappointment.com/getLeads?leadName=${leads}`;
     }
     else {
-        url = `http://localhost:5000/getLeads`;
+        url = `https://backend.janitorialappointment.com/getLeads`;
     }
     const res = await fetch(url);
     if (!res.ok) throw new Error("Failed to fetch data");
