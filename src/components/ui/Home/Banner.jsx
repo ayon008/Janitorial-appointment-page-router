efd6c5d4e154c98fe96de0e30830a261661a2a96
ayon008 <shariar.ayon128@gmail.com>
2025-05-21 14:59:58 +0600

    a

--- src/components/ui/Home/Banner.jsx
+++ src/components/ui/Home/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Pagination, EffectFade } from 'swiper/modules';
 import 'swiper/css';
